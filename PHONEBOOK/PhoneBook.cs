9439ddf828ba3cd3033f5e43efae5ea5ddbe4ac5
devmatdev <devmatdev@gmail.com>
2022-03-13 15:22:23 +0100

    Ksiazka telefoniczna

---
+++ PHONEBOOK/PhoneBook.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace PHONEBOOK
+{
+     class PhoneBook
+    {
+       public List<Contact> ContactList { get; set; } = new List<Contact>();
+       public void AddContact(Contact contact)
+        {
+            ContactList.Add(contact);
+
+            
+        }
+
+        public void ShowContacts()
+        {
+            foreach(Contact con in ContactList)
+                ShowContactDetails(con);
+        }
+        public void ShowContactDetails(Contact contact)
+        {
+            Console.WriteLine($"Contact name: {contact.Name} ; phone number: {contact.Number}");
+        }
+        public void DeleteContact(string name)
+        {
+            ContactList.Remove(ContactList.FirstOrDefault(x => x.Name == name));
+        }
+        public void SortContacts()
+        {
+            ContactList = ContactList.OrderBy(x => x.Name).ToList();
+        }
+        public void FindContact(string name)
+        {
+            ShowContactDetails(ContactList.FirstOrDefault(x => x.Name == name));
+        }
+    }
+}
---
+++ PHONEBOOK/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.IO;
+namespace PHONEBOOK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var phonebook = new PhoneBook();
+            
+            if (!File.Exists("programData.json"))
+            {
+
+            }
+            else
+              phonebook = JsonConvert.DeserializeObject<PhoneBook>(File.ReadAllText("programData.json"));
+         
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to PhoneBook App");
+                Console.WriteLine("Select option by press number: ");
+                Console.WriteLine("1 - Show all contacts");
+                Console.WriteLine("2 - Add contact");
+                Console.WriteLine("3 - Find contact");
+                Console.WriteLine("4 - Delete contact");
+                Console.WriteLine("5 - Exit");
+                string userInput = Console.ReadLine();
+                string name=default;
+                switch (userInput)
+                {
+                    case "1":
+                        {
+                            phonebook.ShowContacts();
+                            Console.WriteLine("To continue press ENTER");
+                            Console.ReadLine();
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Enter the name: ");
+                            name = Console.ReadLine();
+                            Console.WriteLine("Enter the number: ");
+                            var number = Console.ReadLine();
+                            var con = new Contact(name, number);
+                            phonebook.AddContact(con);
+                            phonebook.SortContacts();
+                            var phoneBookSerialized = JsonConvert.SerializeObject(phonebook);
+                            File.WriteAllText("programData.json", phoneBookSerialized);
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.Write("Enter the name of contact you looking for: ");
+                            name = Console.ReadLine();
+                            phonebook.FindContact(name);
+                            Console.WriteLine("To continue press ENTER");
+                            Console.ReadLine();
+                            break;
+                        }
+                    case "4":
+                        {
+                            Console.WriteLine("Enter the name of the contact to be deleted: ");
+                            name = Console.ReadLine();
+                            phonebook.DeleteContact(name);
+                            Console.WriteLine("Contact deleted");
+                            Console.WriteLine("To continue press ENTER");
+                            break;
+                        }
+                    case "5":
+                        {
+
+                            return;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Wrong choice, try again");
+                            Console.WriteLine("To continue press ENTER");
+                            Console.ReadLine();
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
